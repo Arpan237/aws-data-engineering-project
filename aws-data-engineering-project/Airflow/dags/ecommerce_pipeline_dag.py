007c0b64abc442c89ee9ab1c4338bceaa7ebba2d
Arpan237 <99944549+Arpan237@users.noreply.github.com>
2026-01-17 01:07:10 +0530

    Update ecommerce_pipeline_dag.py

--- aws-data-engineering-project/Airflow/dags/ecommerce_pipeline_dag.py
+++ aws-data-engineering-project/Airflow/dags/ecommerce_pipeline_dag.py
@@ -1,43 +1,68 @@
 from airflow import DAG
+from airflow.providers.amazon.aws.operators.glue import GlueJobOperator
+from airflow.providers.snowflake.operators.snowflake import SnowflakeOperator
 from airflow.operators.bash import BashOperator
-from airflow.providers.amazon.aws.operators.glue import AwsGlueJobOperator
-from datetime import datetime
+from airflow.utils.dates import days_ago
+from datetime import datetime, timedelta
 
 default_args = {
     "owner": "airflow",
-    "start_date": datetime(2026, 1, 13),
-    "email": ["your_email@gmail.com"],
+    "depends_on_past": False,
     "email_on_failure": True,
     "email_on_retry": False,
-    "retries": 1
+    "retries": 1,
+    "retry_delay": timedelta(minutes=10),
 }
 
 with DAG(
-    dag_id="ecommerce_end_to_end_pipeline",
-    schedule_interval="@daily",
+    dag_id="ecommerce_data_pipeline",
     default_args=default_args,
-    catchup=False
+    description="End-to-end pipeline: Glue → Snowflake → dbt",
+    schedule_interval="@daily",
+    start_date=datetime(2026, 1, 10),
+    catchup=False,
+    tags=["ecommerce", "aws", "snowflake", "dbt"],
 ) as dag:
 
+# API Ingestion
     api_ingestion = BashOperator(
         task_id="api_to_s3",
         bash_command="python /ingestion/api_to_s3.py"
     )
-
+# Run glue ETL job
     glue_etl = AwsGlueJobOperator(
         task_id="glue_etl",
         job_name="ecommerce-glue-job",
         region_name="us-east-1"
     )
 
+# LOAD INTO SNOWFLAKE (COPY command)
+    snowflake_copy_raw = SnowflakeOperator(
+        task_id="snowflake_copy_raw_data",
+        snowflake_conn_id="snowflake_default",
+        sql="snowflake/copy_into_tables.sql"
+    )
+ # DBT run
     dbt_run = BashOperator(
         task_id="dbt_run",
         bash_command="cd /dbt && dbt run"
     )
-
+# DBT test 
     dbt_test = BashOperator(
         task_id="dbt_test",
         bash_command="cd /dbt && dbt test"
     )
 
-    api_ingestion >> glue_etl >> dbt_run >> dbt_test
+# OPTIONAL: REFRESH EXTERNAL TABLES
+    refresh_external = SnowflakeOperator(
+        task_id="refresh_snowflake_external",
+        snowflake_conn_id="snowflake_default",
+        sql="""
+        alter external table ext_orders refresh;
+        alter external table ext_customers refresh;
+        alter external table ext_products refresh;
+        """
+    )
+
+
+    api_ingestion >> glue_etl >> snowflake_copy_raw_data >> dbt_run >> dbt_test >> refresh_snowflake_external
